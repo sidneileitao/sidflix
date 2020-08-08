22b0a2674648ba60d226bc575a92296666c2fb39
sidneileitao <sidneileitao@gmail.com>
2020-08-08 12:24:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import Carousel from './components/Carousel';
+import dadosIniciais from './dados_iniciais.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+      <h1>Hello Word</h1>
+      Alguma coisa...
     </div>
   );
 }
